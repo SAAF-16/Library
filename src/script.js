dd99ef7edd5e01925239027649e328963d021c19
SAAF-16 <samsnow16@gmail.com>
2021-06-21 21:13:00 +0200

    local storage, comments, various improvements

--- src/script.js
+++ src/script.js
@@ -1,104 +1,283 @@
 class Book {
 
-    constructor(title, pages, description, year, author, genre) {
+    constructor(title, pages, description, year, author, genre, read_pages) {
         this.title = title;
         this.pages = pages;
         this.description = description;
         this.year = year;
         this.author = author;
         this.genre = genre;
+        this.read_pages = read_pages;
     }
 }
-let activeCategory = "Horror";
-const last_added = document.querySelector("#last_added");
-const bAdd_new = document.getElementById("bAdd_new");
+let myLibrary;
+if (!(localStorage.getItem('library'))) {// if the library is in the local storage loads it
+    myLibrary = [];
+} else {
+    myLibrary = JSON.parse(localStorage["library"]);
+}
+let activeCategory = "all";
+let searched_text = false;
+let active_book;
+let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod lectus tellus, vel viverra dolor elementum sed. Praesent non nunc sed tortor laoreet pretium vitae vitae nisl. Cras magna enim, tincidunt sit amet ex nec, fermentum molestie neque.";
+myLibrary[0] = new Book("Carillon", 96, "Long story of a short movie." + loremIpsum, 1984, "John Doe", "Drama", 96);
+myLibrary[1] = new Book("Jemba", 42, "Jemba jemba jemba is the real jemba jemba." + loremIpsum, 2001, "Jemba!", "Horror", 0);
+myLibrary[2] = new Book("The ride", 157, "A long ride taking to never seen spaces." + loremIpsum, 1895, "Carl Carlson", "Drama", 157);
+myLibrary[3] = new Book("To the line", 132, "Everything in this book goes to the line." + loremIpsum, 1995, "Krave Denson", "Adventure", 34);
+myLibrary[4] = new Book("Unforgiven", 204, "Never forget never forgive." + loremIpsum, 1999, "Zaza Zazovis", "Horror", 204);
+myLibrary[5] = new Book("Bojack", 458, loremIpsum, 2005, "Zaza Zazovis", "Romance", 204);
+myLibrary[6] = new Book("Magic Balls", 265, loremIpsum, 1001, "Carl Lewis", "Action", 54);
+myLibrary[7] = new Book("The Glory", 203, loremIpsum, 1658, "Lit Mirrya", "Horror", 203);
+myLibrary[8] = new Book("Zamnesia", 78, loremIpsum, 1864, "Juan Pablo", "Fantasy", 21);
+
+let title = 0;
+let pages = 0;
+let description = 0;
+let year = 0;
+let author = 0;
+let genre = 0;
+
+const show_book = document.getElementById("show_book");
+const show_title = document.getElementById("show_title");
+const show_author = document.getElementById("show_author");
+const show_pages = document.getElementById("show_pages");
+const show_genre = document.getElementById("show_genre");
+const show_years = document.getElementById("show_years");
+const show_description = document.getElementById("show_description");
+const delete_book = document.getElementById("delete_book");
+const mark_read = document.getElementById("mark_read");
+const read_pages = document.getElementById("read_pages");
+const search_text = document.getElementById("search_text");
+const book_categories = document.getElementById("book_categories");
+const category_books = document.getElementById("category_books");
+const search_results = document.getElementById("search_results");
+const last_added = document.getElementById("last_added");
+const button_add_new = document.getElementById("button_add_new");
 const div_new_book = document.getElementById("div_new_book");
 const overlay = document.getElementById("overlay");
-bAdd_new.onclick = () => {
-    div_new_book.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
+const add_new_book = document.getElementById("add_new_book");
+const bottom = document.getElementById("bottom");
+const middle = document.getElementById("middle");
+const total_books = document.getElementById("total_books");
+const finished_books = document.getElementById("finished_books");
+const unfinished_books = document.getElementById("unfinished_books");
+const cancel = document.querySelectorAll(".cancel");
+
+searched = (search_value, i) => {
+    if (search_value == false) return false;
+    return myLibrary[i].title.toLowerCase().includes(search_value.toLowerCase());
 };
 
 
-const book_categories = document.getElementById("book_categories");
-book_categories.addEventListener("click", (e) => {
-    activeCategory = e.target.dataset.category;
-    updateBooks(category_books, 1);
-});
-const category_books = document.getElementById("category_books");
 
 
+button_add_new.addEventListener('click', hideWindows());
+search_text.addEventListener('input', show_search_results());
+read_pages.addEventListener('change', update_read_status());
+book_categories.addEventListener("click", update_active_category());
+read_pages.addEventListener('input', check_valid_input());
+delete_book.addEventListener('click', delete_selected_book());
+add_new_book.addEventListener('click', add_selected_book());
+mark_read.addEventListener('click', mark_as_read());
+cancel.forEach(exit_window());
 
-let myLibrary = [];
-myLibrary[0] = new Book("Carillon", 96, "long story of a short movie", 1984, "John Doe", "Drama");
-myLibrary[1] = new Book("Jemba", 42, "jemba jemba jemba is the real jemba jemba, jemba", 2001, "Jemba!", "Horror");
-myLibrary[2] = new Book("The ride", 157, "a long ride taking to never seen spaces", 1895, "Carl Carlson", "Drama");
-myLibrary[3] = new Book("To the line", 132, "everything in this book goes to the line", 1995, "Krave Denson", "Adventure");
-myLibrary[4] = new Book("Unforgiven", 204, "never forget never forgive", 1999, "Zaza Zazovis", "Horror");
+updateBooks(last_added, 0);         //updates the list of the book in the last added section
+updateBooks(category_books, 1);     //updates the list of book in the categories section
+
+function mark_as_read() {
+    return () => {
+        if (mark_read.checked == true) {
+            read_pages.value = myLibrary[active_book].pages;
+        } else {
+            read_pages.value = 0;
+        }
+    };
+}
+
+function updatelog() {
+    let finished = 0;
+    let unfinished = 0;
+    total_books.textContent = `${myLibrary.length}`;
+    for (i = 0; i < myLibrary.length; i++) {
+        if (myLibrary[i].pages == myLibrary[i].read_pages) {
+            finished++;
+        } else {
+            unfinished++;
+        }
+    }
+    finished_books.textContent = finished;
+    unfinished_books.textContent = unfinished;
+
+}
+let filled = () => {
+    form = [title, pages, description, year, author, genre];
+    for (let i = 0; i < form.length; i++) {
+        if (form[i] == "") {
+            console.log("vuoto");
+            return false;
+        }
+    }
+    console.log("pieno");
+    return true;
+};
+function add_selected_book() {
+
+    return () => {
+
+        title = document.getElementById("bTitle").value;
+        pages = document.getElementById("bPages").value;
+        description = document.getElementById("bDescription").value;
+        year = document.getElementById("bYear").value;
+        author = document.getElementById("bAuthor").value;
+        genre = document.getElementById("bGenre").value;
+        if (!(filled())) return false;
+        hideBookOverlay();
+        addToLibrary(new Book(title, pages, description, year, author, genre));
+        updateBooks(last_added, 0);
+        clearForm();
 
 
+    };
+}
 
+function delete_selected_book() {
+    return () => {
+        myLibrary.splice(active_book, 1);
+        updateBooks(last_added, 0);
+        updateBooks(category_books, 1);
+        hideBookOverlay();
+    };
+}
+
+function exit_window() {
+    return (canc) => {
+        canc.addEventListener('click', () => {
+            hideBookOverlay();
+            clearForm();
+        });
+    };
+}
+
+function check_valid_input() {
+    return (e) => {
+        if (e.target.value > myLibrary[active_book].pages) {
+            e.target.value = myLibrary[active_book].pages;
+        }
+    };
+}
+
+function update_active_category() {
+    return (e) => {
+        document.querySelectorAll(".category").forEach((e) => {
+            e.classList.remove("selected");
+        });
+        activeCategory = e.target.dataset.category;
+        e.target.classList.add("selected");
+
+        updateBooks(category_books, 1);
+    };
+}
+
+function update_read_status() {
+    return () => {
+        myLibrary[active_book].pages == +read_pages.value ?
+            mark_read.checked = true :
+            mark_read.checked = false;
+
+
+    };
+}
+
+function show_search_results() {
+    return (e) => {
+
+        if (e.target.value != "") {
+            activeCategory = -1;
+            searched_text = e.target.value;
+            search_results.classList.remove('not_searching');
+            updateBooks(search_results, 1);
+        }
+
+        if (e.target.value == "") {
+            activeCategory = "all";
+            search_results.classList.add("not_searching");
+            searched_text = false;
+        }
+    };
+}
+
+function hideWindows() {
+    return () => {
+        div_new_book.classList.toggle("hidden");
+        overlay.classList.toggle("hidden");
+    };
+}
+
+function hideBookOverlay() {
+    div_new_book.classList.add("hidden");
+    overlay.classList.add("hidden");
+    show_book.classList.add("hidden");
+}
+
+function clearForm() {
+    div_new_book.reset();
+}
 
 function addToLibrary(book) {
     if (!(book instanceof Book)) return false;
     myLibrary[myLibrary.length] = book;
 }
 
-
-updateBooks(last_added, 0);
-updateBooks(category_books, 1);
-
-
-
 function updateBooks(doc, active) {
     doc.innerHTML = "";
+    let book = 0;
     for (let i = myLibrary.length - 1; i >= 0; i--) {
-        if ((active == 0) || (activeCategory == "all") || (`${myLibrary[i].genre}` == activeCategory)) {
-            let book = document.createElement("div");
+        //the next if allows me to use the same funcion for different purposes
+        if ((active == 0) ||                                // -if active is set to 0 we are just updating the last added books
+            (activeCategory == "all") ||                       // -if = 'all' show all categories
+            (`${myLibrary[i].genre}` == activeCategory) ||     // -show only desired category
+            (searched(searched_text, i)))                      // -show only if contains searched text
+        {
+            book = document.createElement("div");
             book.classList.add("book");
             book.addEventListener("click", (e) => {
-                console.log(e.target);
+                active_book = i;
+                show_title.textContent = `${myLibrary[i].title}`;
+                show_author.textContent = `${myLibrary[i].author}`;
+                show_pages.textContent = `${myLibrary[i].pages}`;
+                show_genre.textContent = `${myLibrary[i].genre}`;
+                show_years.textContent = `${myLibrary[i].year}`;
+                show_description.textContent = `${myLibrary[i].description}`;
+                read_pages.value = `${myLibrary[i].read_pages}`;
+                if (myLibrary[i].read_pages == myLibrary[i].pages) {
+                    mark_read.checked = true;
+                } else {
+                    mark_read.checked = false;
+                }
+
+                show_book.classList.toggle("hidden");
+
+                overlay.classList.toggle("hidden");
+
             });
             book.textContent = `${myLibrary[i].title}`;
             doc.appendChild(book);
         }
+    }
+    if (active == 1) {          //if active=1 ( we are in the categories area) sort alphabetically
+        [...doc.children]
+            .sort((a, b) => {
+                if (a.textContent > b.textContent) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            })
+            .forEach(node => doc.appendChild(node));
 
 
     }
-}
-const cancel = document.getElementById("cancel");
-cancel.onclick = function () {
-    hideBookOverlay();
-    clearForm();
-};
-const add_new_book = document.getElementById("add_new_book");
-add_new_book.onclick = function () {
-
-    let title = document.getElementById("bTitle").value;
-    let pages = document.getElementById("bPages").value;
-    let description = document.getElementById("bDescription").value;
-    let year = document.getElementById("bYear").value;
-    let author = document.getElementById("bAuthor").value;
-    let genre = document.getElementById("bGenre").value;
-
-    addToLibrary(new Book(title, pages, description, year, author, genre));
-    updateBooks();
-
-    hideBookOverlay();
-
-    clearForm();
-};
-function hideBookOverlay() {
-    div_new_book.classList.toggle("hidden");
-    overlay.classList.toggle("hidden");
-}
-
-function clearForm() {
-    document.getElementById("bTitle").value = "";
-    document.getElementById("bPages").value = "";
-    document.getElementById("bDescription").value = "";
-    document.getElementById("bYear").value = "";
-    document.getElementById("bAuthor").value = "";
-    document.getElementById("bGenre").value = "";
+    updatelog();
+    localStorage.setItem('library', JSON.stringify(myLibrary));
 }
 
