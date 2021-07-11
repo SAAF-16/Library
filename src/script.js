51a10cad6383e614e9a6b8c733f0b6c3d4f7f7b7
SAAF-16 <samsnow16@gmail.com>
2021-07-11 13:38:22 +0200

    added read status

--- src/script.js
+++ src/script.js
@@ -19,12 +19,13 @@ if (!(localStorage.getItem('library'))) {// if the library is in the local stora
 let activeCategory = "all";
 let searched_text = false;
 let active_book;
+let read_status;
 let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod lectus tellus, vel viverra dolor elementum sed. Praesent non nunc sed tortor laoreet pretium vitae vitae nisl. Cras magna enim, tincidunt sit amet ex nec, fermentum molestie neque.";
-myLibrary[0] = new Book("Carillon", 96, "Long story of a short movie." + loremIpsum, 1984, "John Doe", "Drama", 96);
-myLibrary[1] = new Book("Jemba", 42, "Jemba jemba jemba is the real jemba jemba." + loremIpsum, 2001, "Jemba!", "Horror", 0);
-myLibrary[2] = new Book("The ride", 157, "A long ride taking to never seen spaces." + loremIpsum, 1895, "Carl Carlson", "Drama", 157);
-myLibrary[3] = new Book("To the line", 132, "Everything in this book goes to the line." + loremIpsum, 1995, "Krave Denson", "Adventure", 34);
-myLibrary[4] = new Book("Unforgiven", 204, "Never forget never forgive." + loremIpsum, 1999, "Zaza Zazovis", "Horror", 204);
+myLibrary[0] = new Book("Carillon", 96, loremIpsum, 1984, "John Doe", "Drama", 96);
+myLibrary[1] = new Book("Jemba", 42, loremIpsum, 2001, "Jemba!", "Horror", 0);
+myLibrary[2] = new Book("The ride", 157, loremIpsum, 1895, "Carl Carlson", "Drama", 157);
+myLibrary[3] = new Book("To the line", 132, loremIpsum, 1995, "Krave Denson", "Adventure", 34);
+myLibrary[4] = new Book("Unforgiven", 204, loremIpsum, 1999, "Zaza Zazovis", "Horror", 204);
 myLibrary[5] = new Book("Bojack", 458, loremIpsum, 2005, "Zaza Zazovis", "Romance", 204);
 myLibrary[6] = new Book("Magic Balls", 265, loremIpsum, 1001, "Carl Lewis", "Action", 54);
 myLibrary[7] = new Book("The Glory", 203, loremIpsum, 1658, "Lit Mirrya", "Horror", 203);
@@ -81,16 +82,22 @@ add_new_book.addEventListener('click', add_selected_book());
 mark_read.addEventListener('click', mark_as_read());
 cancel.forEach(exit_window());
 
-updateBooks(last_added, 0);         //updates the list of the book in the last added section
-updateBooks(category_books, 1);     //updates the list of book in the categories section
+refreshBooks();
+
+function refreshBooks() {
+    updateBooks(last_added, 0); //updates the list of the book in the last added section
+    updateBooks(category_books, 1); //updates the list of book in the categories section
+}
 
 function mark_as_read() {
     return () => {
         if (mark_read.checked == true) {
             read_pages.value = myLibrary[active_book].pages;
+            myLibrary[active_book].read_pages = myLibrary[active_book].pages;
         } else {
             read_pages.value = 0;
         }
+        refreshBooks();
     };
 }
 
@@ -134,7 +141,7 @@ function add_selected_book() {
         hideBookOverlay();
         addToLibrary(new Book(title, pages, description, year, author, genre));
         updateBooks(last_added, 0);
-        clearForm();
+        // clearForm();
 
 
     };
@@ -153,7 +160,7 @@ function exit_window() {
     return (canc) => {
         canc.addEventListener('click', () => {
             hideBookOverlay();
-            clearForm();
+
         });
     };
 }
@@ -180,11 +187,13 @@ function update_active_category() {
 
 function update_read_status() {
     return () => {
-        myLibrary[active_book].pages == +read_pages.value ?
-            mark_read.checked = true :
+        if (myLibrary[active_book].pages == +read_pages.value) {
+            mark_read.checked = true;
+        } else {
             mark_read.checked = false;
-
-
+        }
+        myLibrary[active_book].read_pages = +read_pages.value;
+        refreshBooks();
     };
 }
 
@@ -232,15 +241,15 @@ function updateBooks(doc, active) {
     doc.innerHTML = "";
     let book = 0;
     for (let i = myLibrary.length - 1; i >= 0; i--) {
-        //the next if allows me to use the same funcion for different purposes
-        if ((active == 0) ||                                // -if active is set to 0 we are just updating the last added books
+        //the next if allows to use the same funcion for different purposes
+        if ((active == 0) ||                                   // -if active is set to 0 we are just updating the last added books
             (activeCategory == "all") ||                       // -if = 'all' show all categories
             (`${myLibrary[i].genre}` == activeCategory) ||     // -show only desired category
             (searched(searched_text, i)))                      // -show only if contains searched text
         {
             book = document.createElement("div");
             book.classList.add("book");
-            book.addEventListener("click", (e) => {
+            book.addEventListener("click", () => {
                 active_book = i;
                 show_title.textContent = `${myLibrary[i].title}`;
                 show_author.textContent = `${myLibrary[i].author}`;
@@ -254,13 +263,22 @@ function updateBooks(doc, active) {
                 } else {
                     mark_read.checked = false;
                 }
-
                 show_book.classList.toggle("hidden");
-
                 overlay.classList.toggle("hidden");
+                clearForm();
 
             });
             book.textContent = `${myLibrary[i].title}`;
+            read_status = document.createElement("p");
+            read_status.classList.add("status");
+            if (myLibrary[i].read_pages == myLibrary[i].pages) {
+                read_status.classList.add("finished");
+            } else if (myLibrary[i].read_pages == 0) {
+                read_status.classList.add("not_started");
+            } else {
+                read_status.classList.add("reading");
+            }
+            book.appendChild(read_status);
             doc.appendChild(book);
         }
     }
